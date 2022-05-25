30e9858490d31d59e8f39d46c2804dafaefe13e6
Hadeer <hadeersalah127@gmail.com>
2022-05-25 22:23:03 +0200

    h1

--- src/app/pages/post/add-post/add-post.component.ts
+++ src/app/pages/post/add-post/add-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostService } from 'src/app/service/post.service';
 
 @Component({
   selector: 'app-add-post',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent implements OnInit {
+count = 1;
+  isLiked = false
 
-  constructor() { }
+  likeButton = () => {
+    if(this.isLiked) {
+      this.count--
+    console.log('no like')
+    }
+
+    else
+    this.count++
+  }
+  constructor(private _post:PostService) { }
 
   ngOnInit(): void {
   }
 
+  newPost(post: NgForm){
+    if(post.valid){
+      this._post.newPost(post.value).subscribe(
+        data => console.log(data),
+        (e) => console.log(e.message),
+        () => console.log ("hello")
+        )
+    }
+  }
 }
--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  host2 = "http://localhost:3000/post/"
+
+
+  constructor(private _myHttp:HttpClient) { }
+
+  newPost(data: any): Observable<any>{
+    return this._myHttp.post(`${this.host2}add`, data)
+    }
 }
